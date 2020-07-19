c141d5f2f8a2f8ad1ce84cf7e25a40353baef724
qmxwby <2621955028@qq.com>
2020-07-19 22:17:00 +0800

    写完HelloWorld组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
@@ -25,12 +16,17 @@ export default {
 </script>
 
 <style>
+body{
+	margin: 0;
+	padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+  padding: 0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+	<el-container>
+	  <el-header style="text-align: left">
+		<el-row type="flex" align="middle" style="height: 100%;">
+			<el-col :span="3"></el-col>
+			<el-col :span="11" class="loc">
+				<i class="el-icon-location"></i>
+				宜昌
+				<i class="el-icon-arrow-down" style="margin-right: 30px;"></i>
+				<span style="color: #2C3E50;">定位所在地</span>
+			</el-col>
+			<el-col :span="10" style="font-size: 14px; color: #000;">
+				<el-row>
+					<el-col :span="3">
+						<i class="el-icon-user"></i>
+						登入
+					</el-col>
+					<el-col :span="3">
+						<i class="el-icon-female"></i>
+						注册
+					</el-col>
+					<el-col :span="4">
+						<i class="el-icon-mobile-phone"></i>
+						手机版本
+					</el-col>
+					<el-col :span="3">					
+						公众号
+						<i class="el-icon-arrow-down"></i>
+					</el-col>
+					<el-col :span="3">
+						网站导航
+					</el-col>
+				</el-row>
+			</el-col>
+		</el-row>
+	  </el-header>	  
+	  <el-main style="text-align: left;">
+		<el-row type="flex">
+			<el-col :span="2"></el-col>
+			<el-col :span="4" style="margin-bottom: 20px;">
+				<img style="width: 80%;" src="../assets/icon.png"/>
+			</el-col>
+			<el-col :span="11" style="padding-top: 30px;">
+				<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+				  <el-menu-item index="1" class="sub1">首页</el-menu-item>
+				  <el-submenu index="2" style="width: 25%;" class="sub2">
+				    <template slot="title">援助类型</template>
+				    <el-menu-item index="2-1">选项1</el-menu-item>
+				    <el-menu-item index="2-2">选项2</el-menu-item>
+				    <el-menu-item index="2-3">选项3</el-menu-item>
+				  </el-submenu>
+				  <el-submenu index="3" style="width: 25%;" class="sub3">
+					 <template slot="title">援助途径</template>
+					 <el-menu-item index="3-1">选项1</el-menu-item>
+					 <el-menu-item index="3-2">选项2</el-menu-item>
+					 <el-menu-item index="3-3">选项3</el-menu-item> 
+				  </el-submenu>
+				  <el-menu-item index="4" class="sub1">预约律师</el-menu-item>
+				</el-menu>
+			</el-col>
+			<el-col :span="5" class="input_s">
+				<el-input v-model="input" placeholder="请输入你想输入的内容..."></el-input>
+				<el-button icon="el-icon-search" circle></el-button>
+			</el-col>
+			<el-col :span="2"></el-col>
+		</el-row>
+	  </el-main>
+	</el-container>
   </div>
+    
 </template>
 
 <script>
@@ -35,24 +76,78 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+	  input: ''
+   }
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.el-header {
+/* 	padding: 0 10%; */
+	background: rgb(240,240,240);
+	background-color: #f8f8f8;
+	border-color: #e7e7e7;
+	border: 1px solid transparent;
+	border-radius: 4px;
+	max-height: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.loc {
+	font-size: 14px;
+	color: #2b73c8;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.el-main {
+	background-color:#fff; 
+	height: auto;
+	box-shadow: 0px 7px 10px 0px  rgba(195,195,195,0.5);
+	padding: 20px 20px 0 20px;
 }
-a {
-  color: #42b983;
+.sub1{
+	color: #000 !important;
+	font-size: 18px;
+	font-weight: 500;
+	width: 25%;
+}
+.sub1:hover{
+	color: #69AF2F !important;
+}
+.sub2 >>> .el-submenu__title{
+	color: #000 !important;
+	font-size: 18px;
+	font-weight: 500;
+}
+.sub2 >>> .el-submenu__title:hover{
+	color: #69af2f !important;
+}
+.sub2 >>> .el-submenu__title i{
+	font-size: 18px;
+}
+.sub3 >>> .el-submenu__title{
+	color: #000 !important;
+	font-size: 18px;
+	font-weight: 500;
+}
+.sub3 >>> .el-submenu__title:hover{
+	color: #69af2f !important;
+}
+.sub3 >>> .el-submenu__title i{
+	font-size: 18px;
+}
+.input_s >>> .el-input {
+	width: 75%;
+}
+.input_s >>> input{
+	border-radius: 20px;
+	background-color: #FAECE1;
+}
+.input_s >>> .el-button {
+	color: #2EA642;
+	font-size: 20px;
+	border-color: #fff;
 }
 </style>
